b3f6c22dc3bf286e74a42a7b98cd11b44e263b21
Alexis He <ahe@slb.com>
2017-07-27 15:05:52 +0200

    Add: drawing example of a line on canvas

--- script.js
+++ script.js
@@ -5,14 +5,41 @@ define(["jquery", "geotoolkit"], function(){
     var initialize = function() {
         // initialization function of the module
         console.log('initialized');
+
+        // Create a shape and define its properties
+        var line = new geotoolkit.scene.shapes.Line({
+            'from' : new geotoolkit.util.Point(0, 0),
+            'to' : new geotoolkit.util.Point(200, 200),
+            'linestyle' : new geotoolkit.attributes.LineStyle()
+        }); 
+
+        // Create a group to hold nodes
+        var group = new geotoolkit.scene.Group();
+
+        // Add the shape to the group
+        group.addChild(line);
+
+        // Get the canvas as a DOM object
+        var canvas = document.getElementById("tutorial-canvas");
+
+        // Create a new Plot object from the canvas and group
+        return new geotoolkit.plot.Plot({
+            'canvasElement' : canvas,
+            'root' : group
+        });
     }
 
     ///////////////////////////////////////////////////////////////
     // Module body
     ///////////////////////////////////////////////////////////////
+    var start = function()
+    {
+        console.log('doing nothing')
+    }
 
     // When this module is loaded into html, or other module, this object is returned
     return {
-        'initialize': initialize
+        'initialize': initialize,
+        'start': start
     }
 });
