78f2845b92c174a400fa9122674a5e582e4a65a9
Alexis He <ahe@slb.com>
2017-07-27 19:15:26 +0200

    Refactor: separate function for adding objetcs to plot

--- script.js
+++ script.js
@@ -18,46 +18,13 @@ define(["jquery", "geotoolkit", "geotoolkit.data", "geotoolkit.controls"], funct
             'root' : rootGroup
         });
 
-        // axis example
-        var tickAxis = new geotoolkit.axis.AdaptiveTickGenerator();
-        var boundsAxis = new geotoolkit.util.Rect(10, 0, 50, 400);
-        var modelLimits = new geotoolkit.util.Rect(0, 0, 600, 600);
-        var axis = new geotoolkit.axis.Axis(tickAxis)
-                .setBounds(boundsAxis)
-                .setModelLimits(modelLimits)
-                .setAutoLabelRotation(true);
+        var axis = buildAxisExample();
+        var line = buildLineExample();
+        var grid = buildGridExample();
 
-        // line example
-        var line = new geotoolkit.scene.shapes.Line({
-            'from' : new geotoolkit.util.Point(0, 0),
-            'to' : new geotoolkit.util.Point(600, 600),
-            'linestyle' : new geotoolkit.attributes.LineStyle()
-        });
+        var crossHair = buildCrosshairExample(rootGroup);
+        var panning = buildPanningExample();
 
-        // grid example
-        var hTickGrid = new geotoolkit.axis.AdaptiveTickGenerator();
-        var vTickGrid = new geotoolkit.axis.AdaptiveTickGenerator();
-        var boundsGrid = new geotoolkit.util.Rect(0, 0, 600, 600);
-        var grid = new geotoolkit.axis.Grid(hTickGrid, vTickGrid);
-
-        // crosshair example
-        var crossHairSettings = {
-            'vertical': new geotoolkit.attributes.LineStyle(),
-            'horizontal': new geotoolkit.attributes.LineStyle(),
-            'group': rootGroup
-        };
-        var crossHair = new geotoolkit.controls.tools.CrossHair(crossHairSettings);
-        crossHair.setEnabled(false);
-
-        // panning example
-        var panning = new geotoolkit.controls.tools.Panning()
-            .setEnabled(true)
-            .addListener(geotoolkit.controls.tools.Panning.Events.onPanning, function (sender, eventArgs) {
-                var diff = eventArgs.getDirection();
-                console.log(diff);
-            }.bind(this));
-
-        // Add the shape to the group
         rootGroup.addChild(line);
         rootGroup.addChild(axis);
         rootGroup.addChild(grid);
@@ -69,31 +36,82 @@ define(["jquery", "geotoolkit", "geotoolkit.data", "geotoolkit.controls"], funct
         return plot;
     }
 
-    /*
-    function CreatePanning() {
-        var panningAnnotated = createAnnotatedNode();
-        var panning = new geotoolkit.controls.tools.Panning()
-            .setEnabled(true)
-            .addListener(geotoolkit.controls.tools.Panning.Events.onPanning, function (sender, eventArgs) {
-                var diff = eventArgs.getDirection();
-                panningAnnotated.translateModel(diff.x, diff.y);
-            }.bind(this));
-
-        var plot = addShapeToCanvas('PanningCanvas', panningAnnotated);
-        new geotoolkit.controls.tools.ToolsContainer(plot)
-            .add(
-                new geotoolkit.controls.tools.CompositeTool(panningAnnotated.getModel(),"compositeNode")
-                    .add(panning)
-            );
-    }
-        */
-
     ///////////////////////////////////////////////////////////////
     // Module body
     ///////////////////////////////////////////////////////////////
     var start = function()
     {
         console.log('doing nothing')
+    }
+
+    var buildAxisExample = function()
+    {
+        // axis example
+        var tickAxis = new geotoolkit.axis.AdaptiveTickGenerator();
+        var boundsAxis = new geotoolkit.util.Rect(10, 0, 50, 400);
+        var modelLimits = new geotoolkit.util.Rect(0, 0, 600, 600);
+        return new geotoolkit.axis.Axis(tickAxis)
+                .setBounds(boundsAxis)
+                .setModelLimits(modelLimits)
+                .setAutoLabelRotation(true);
+    }
+
+    var buildLineExample = function()
+    {
+        // line example
+        return new geotoolkit.scene.shapes.Line({
+            'from' : new geotoolkit.util.Point(0, 0),
+            'to' : new geotoolkit.util.Point(600, 600),
+            'linestyle' : new geotoolkit.attributes.LineStyle()
+        });
+    }
+
+    var buildGridExample = function()
+    {
+        var hTickGrid = new geotoolkit.axis.AdaptiveTickGenerator();
+        var vTickGrid = new geotoolkit.axis.AdaptiveTickGenerator();
+        var boundsGrid = new geotoolkit.util.Rect(0, 0, 600, 600);
+        return new geotoolkit.axis.Grid(hTickGrid, vTickGrid);
+    }
+
+    var buildCrosshairExample = function(group)
+    {
+        var crossHairSettings = {
+            'vertical': new geotoolkit.attributes.LineStyle(),
+            'horizontal': new geotoolkit.attributes.LineStyle(),
+            'group': group
+        };
+        return new geotoolkit.controls.tools.CrossHair(crossHairSettings)
+            .setEnabled(true);
+    }
+
+    var buildPanningExample = function()
+    {
+        return new geotoolkit.controls.tools.Panning()
+            .setEnabled(true)
+            .addListener(geotoolkit.controls.tools.Panning.Events.onPanning, function (sender, eventArgs) {
+                var diff = eventArgs.getDirection();
+                console.log(diff);
+            }.bind(this));
+
+        /*
+        function CreatePanning() {
+            var panningAnnotated = createAnnotatedNode();
+            var panning = new geotoolkit.controls.tools.Panning()
+                .setEnabled(true)
+                .addListener(geotoolkit.controls.tools.Panning.Events.onPanning, function (sender, eventArgs) {
+                    var diff = eventArgs.getDirection();
+                    panningAnnotated.translateModel(diff.x, diff.y);
+                }.bind(this));
+
+            var plot = addShapeToCanvas('PanningCanvas', panningAnnotated);
+            new geotoolkit.controls.tools.ToolsContainer(plot)
+                .add(
+                    new geotoolkit.controls.tools.CompositeTool(panningAnnotated.getModel(),"compositeNode")
+                        .add(panning)
+                );
+        }
+        */
     }
 
     // When this module is loaded into html, or other module, this object is returned
