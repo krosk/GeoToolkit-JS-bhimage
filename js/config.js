5e3be85b8be77959991a0d36cc0132e72fc545a7
Alexis He <ahe@slb.com>
2017-08-01 10:36:11 +0200

    Add: track gen & droidscript comp

---
+++ js/config.js
@@ -0,0 +1,38 @@
+// Contents of config.js, basic require configuration
+require.config({
+    // Alias name for files and paths to them
+    paths: {
+        // Note that .js extension is not specified, it is applied automatically
+        'geotoolkit': 'GeoToolkit-JS/bin/geotoolkit.adv',
+        'geotoolkit.data': 'GeoToolkit-JS/bin/geotoolkit.data.adv',
+        'geotoolkit.controls': 'GeoToolkit-JS/bin/geotoolkit.controls.adv',
+        'jquery': 'GeoToolkit-JS/3rdparty/js/jquery.min',
+    },
+    // Browser independent configuration of the dependencies, exports, and custom init
+    shim: {
+    }
+});// Contents of config.js, basic require configuration
+require.config({
+    // Alias name for files and paths to them
+    paths: {
+        // Note that .js extension is not specified, it is applied automatically
+        'geotoolkit': 'GeoToolkit-JS/bin/geotoolkit.adv',
+        'geotoolkit.data': 'GeoToolkit-JS/bin/geotoolkit.data.adv',
+        'geotoolkit.controls': 'GeoToolkit-JS/bin/geotoolkit.controls.adv',
+        'geotoolkit.welllog': 'GeoToolkit-JS/bin/geotoolkit.welllog.adv',
+        'geotoolkit.welllog.data': 'GeoToolkit-JS/bin/geotoolkit.welllog.data.adv',
+        'geotoolkit.welllog.header': 'GeoToolkit-JS/bin/geotoolkit.welllog.header.adv',
+        'jquery': 'GeoToolkit-JS/3rdparty/js/jquery.min',
+    },
+    // Browser independent configuration of the dependencies, exports, and custom init
+    shim: {
+    }
+});
+
+/*
+var Util = namespace('geotoolkit.util');
+var Scene = namespace('geotoolkit.scene');
+var Control = namespace('geotoolkit.plot');
+var Selection = namespace('geotoolkit.selection');
+var Attributes = namespace('geotoolkit.attributes');
+*/
